d85813d64c2c32d4e271a15a69af66bf7a681a2c
VictorMusta <58694074+VictorMusta@users.noreply.github.com>
2024-03-25 11:35:30 +0100

    Added Jump

--- Assets/CustomAssets/Scripts/MouvementScript.cs
+++ Assets/CustomAssets/Scripts/MouvementScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MouvementScript : MonoBehaviour
@@ -8,10 +9,11 @@ public class MouvementScript : MonoBehaviour
     public Rigidbody rb;
     public float sideMouvementForce = 400f;
     public float forwardForce = 2000f;
-    
+    public float jumpForce = 500f;
+    public int jumpCount = 1;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, forwardForce * Time.deltaTime);
         if (Input.GetKey("d"))
@@ -23,6 +25,33 @@ public class MouvementScript : MonoBehaviour
         {
             rb.AddForce(-sideMouvementForce* Time.deltaTime,0, 0 , ForceMode.Force);
 
+        }
+        if (Input.GetKey(KeyCode.Space) && jumpCount>0)
+        {
+            rb.AddForce(0,jumpForce* Time.deltaTime, 0 , ForceMode.Impulse);
+            jumpCount -= 1;
+        }
+        if (Input.GetKey("r"))
+        {
+            ResetGame();
+        }
+    }
+
+    private void ResetGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/SampleScene");
+
+    }
+    private void OnCollisionEnter(Collision collisionEvent)
+    {
+        if (collisionEvent.gameObject.name == "Ground")
+        {
+            jumpCount = 1;
+        }
+        if (collisionEvent.gameObject.name == "Obstacles")
+        {
+            ResetGame();
+            Debug.Log("GAME OVER");
         }
     }
 }
